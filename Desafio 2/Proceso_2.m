baa414c24952dcf205324f3fcae6b4ccc9103a7b
NotName38 <alexbelcal88@gmail.com>
2020-10-01 20:11:22 +0200

    Revert "Desafio 2: Proceso_1,_2,_3,_4"
    This reverts commit 9b049a6bc11d4064429bf1205f84040ac4728c15.

--- Desafio 2/Proceso_2.m
+++ Desafio 2/Proceso_2.m
@@ -1,53 +1,53 @@
-% Desafio 2 proceso 2
+%% Desafio 2 Proceso 2
+% Set de imagenes: 1,3,5,7,9
+% Operaciones realizadas: 
+%   Strel de un tamaño n con esfera.
+%   imopen con el strel.
+
+%% Inicializaciones:
 close all;
 clc;
-% Inicializaciones.
-addpath("binarizados", "proceso2");
+clear;
+addpath("binarizados", "proceso1");
 binario_1 = imread("binarizados/martillo1.jpg");
 procesado_1 = imread("proceso2/martillo1.jpg");
-binario_2 = imread("binarizados/martillo2.jpg");
-procesado_2 = imread("proceso2/martillo2.jpg");
 binario_3 = imread("binarizados/martillo3.jpg");
 procesado_3 = imread("proceso2/martillo3.jpg");
-binario_4 = imread("binarizados/martillo4.jpg");
-procesado_4 = imread("proceso2/martillo4.jpg");
 binario_5 = imread("binarizados/martillo5.jpg");
 procesado_5 = imread("proceso2/martillo5.jpg");
-binario_6 = imread("binarizados/martillo6.jpg");
-procesado_6 = imread("proceso2/martillo6.jpg");
 binario_7 = imread("binarizados/martillo7.jpg");
 procesado_7 = imread("proceso2/martillo7.jpg");
-binario_8 = imread("binarizados/martillo8.jpg");
-procesado_8 = imread("proceso2/martillo8.jpg");
 binario_9 = imread("binarizados/martillo9.jpeg");
 procesado_9 = imread("proceso2/martillo9.jpeg");
 
+% Valor de n:
+n = 7; % Rangos en los que hay una n buena = [5,6,7,..,10]
 
-% Visualizacion inicial:
+%% Calculo:
+
+aux1 = imopen(binario_1, strel("sphere", n));
+aux3 = imopen(binario_3, strel("sphere", n));
+aux5 = imopen(binario_5, strel("sphere", n));
+aux7 = imopen(binario_7, strel("sphere", n));
+aux9 = imopen(binario_9, strel("sphere", n));
+
+%% Calculo del error:
+error1 = sum(sum(1-(aux1 == procesado_1))); 
+error3 = sum(sum(1-(aux3 == procesado_3))); 
+error5 = sum(sum(1-(aux5 == procesado_5))); 
+error7 = sum(sum(1-(aux7 == procesado_7))); 
+error9 = sum(sum(1-(aux9 == procesado_9)));
+
+%% Visualizacion de resultados:
+
 figure(1);
-subplot(6,3,1); imshow(binario_1);
-subplot(6,3,2); imshow(binario_2);  
-subplot(6,3,3); imshow(binario_3); 
-subplot(6,3,4); imshow(procesado_1)
-subplot(6,3,5); imshow(procesado_2)  
-subplot(6,3,6); imshow(procesado_3) 
-
-subplot(6,3,7); imshow(binario_4);
-subplot(6,3,8); imshow(binario_5);
-subplot(6,3,9); imshow(binario_6);
-subplot(6,3,10); imshow(procesado_4)
-subplot(6,3,11); imshow(procesado_5)  
-subplot(6,3,12); imshow(procesado_6) 
-
-subplot(6,3,13); imshow(binario_7);
-subplot(6,3,14); imshow(binario_8); 
-subplot(6,3,15); imshow(binario_9);
-subplot(6,3,16); imshow(procesado_7)
-subplot(6,3,17); imshow(procesado_8)  
-subplot(6,3,18); imshow(procesado_9) 
-
-aux = imclose((1-imbinarize(binario_1)),strel('square',9));
-figure(2); 
-subplot(1,2,1); imshow(1 - aux)
-subplot(1,2,2); imshow(procesado_1)
-
+subplot(2,5,1); imshow(aux1); title(error1)
+subplot(2,5,2); imshow(aux3); title(error3) 
+subplot(2,5,3); imshow(aux5); title(error5)  
+subplot(2,5,4); imshow(aux7); title(error7)
+subplot(2,5,5); imshow(aux9); title(error9) 
+subplot(2,5,6); imshow(procesado_1) 
+subplot(2,5,7); imshow(procesado_3) 
+subplot(2,5,8); imshow(procesado_5)  
+subplot(2,5,9); imshow(procesado_7) 
+subplot(2,5,10); imshow(procesado_9) 
